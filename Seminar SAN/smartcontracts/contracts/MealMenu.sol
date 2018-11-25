ca1c9c19d3312a8994f2120221c7bc071b366e66
Niels Rood <n.rood@bluejayeindhoven.nl>
2018-11-25 20:43:05 +0100

    Made initial version of smart contract compilable

--- Seminar SAN/smartcontracts/contracts/MealMenu.sol
+++ Seminar SAN/smartcontracts/contracts/MealMenu.sol
@@ -13,36 +13,44 @@ contract MealMenu {
 		uint Price; //in wei, needs conversion to Ether (or currency) in front-end
 		uint8 Capacity; //support <256
 
-		bool Reserved;
-		address Eater;
+		address[] Eaters;
     }
 
-    Meal[] public availableMeals;
+    Meal[] public availableMeals; // use generated getter(?) for getMeals in front-end
 
-    function getMeals() public returns(Meal[]){
-		return availableMeals;
-    }
-
-    function getMeal(uint id) public returns(Meal) {
-		return availableMeals[id];
+    function getMeal(uint id) public view returns (
+    	bytes32 Title, 
+    	string Description, 
+    	bytes32 Where, 
+    	uint When, 
+    	uint Price, 
+    	uint8 Capacity,
+    	uint8 Reserved) {
+    	Title = availableMeals[id].Title;
+    	Description = availableMeals[id].Description;
+    	Where = availableMeals[id].Where;
+    	When = availableMeals[id].When;
+    	Price = availableMeals[id].Price;
+    	Capacity = availableMeals[id].Capacity;
+    	Reserved = uint8(availableMeals[id].Eaters.length);
     }
 
     function createMeal(bytes32 t, string d, bytes32 wr, uint wn, uint p, uint8 c) public {
-    	availableMeals.push(Meal({
-    			Cook: msg.sender,
-    			Title: t,
-    			Description: d,
-    			Where: wr,
-    			When: wn,
-    			Price: p,
-    			Capacity: c,
-    			Reserved: false,
-    			Eater: address(0)
-    		}));
+    	Meal memory m;
+    	m.Cook = msg.sender;
+    	m.Title = t;
+    	m.Description = d;
+    	m.Where = wr;
+    	m.When = wn;
+    	m.Price = p;
+    	m.Capacity = c;
+    	availableMeals.push(m);
     }
 
     function updateMeal(uint id, bytes32 t, string d, bytes32 wr, uint8 c) public {
     	require(msg.sender == availableMeals[id].Cook, "Only a the Cook of this meal can update the meal");
+    	// Do not allow lowering capacity below current reservation count
+    	require(uint8(availableMeals[id].Eaters.length) < c, "You cannot lower the capacity below the number of reservations"); 
     	availableMeals[id].Title = t;
     	availableMeals[id].Description = d;
     	availableMeals[id].Where = wr;
@@ -50,10 +58,9 @@ contract MealMenu {
     }
 
     function reserve(uint id) payable public {
-    	require(!availableMeals[id].Reserved);
+    	require(uint8(availableMeals[id].Eaters.length) < availableMeals[id].Capacity, "The capacity of this meal has been reached");
     	pendingWithdrawals[availableMeals[id].Cook] += msg.value;
-    	availableMeals[id].Reserved = true;
-    	availableMeals[id].Eater = msg.sender;
+    	availableMeals[id].Eaters.push(msg.sender);
     }
 
     // https://solidity.readthedocs.io/en/v0.5.0/common-patterns.html#withdrawal-from-contracts
