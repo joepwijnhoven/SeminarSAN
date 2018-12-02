40892d1a37b2fdbf4e4e55831751fdbe481c2298
Niels Rood <n.rood@bluejayeindhoven.nl>
2018-12-02 16:30:54 +0100

    made fetching of meals list synced to avoid modification conflicts in cache mutations

--- Seminar SAN/frontend/www/js/contract_template.js
+++ Seminar SAN/frontend/www/js/contract_template.js
@@ -270,40 +270,38 @@ async function getMeals() {
   // clearing cache before fetching meals; TODO probably want to do some smart caching here...
   cache.set("meals", []);
 
-  contractInstance.getNumberOfMeals(function(err, result) {
+  contractInstance.getNumberOfMeals(async function(err, result) {
     for (let i = 0; i < result.toNumber(); i++) {
       //push every meal onto the view (do something about order?)
-      contractInstance.getMeal(i, function(error, result) {
-        var mealResult = result;
-        var meal = {};
-        meal.id = i;
-        meal.cook = mealResult[0];
-        meal.title = mealResult[1];
-        meal.description = mealResult[2];
-        meal.place = mealResult[3];
-        meal.time = mealResult[4].toNumber();
-        meal.price = mealResult[5].toNumber();
-        meal.capacity = mealResult[6].toNumber();
-        meal.reservations = mealResult[7];
-        // add the new meal to the meals cache, sorting the list by date 
-        // - for future meals put soon-est first
-        // - for past meals, put latest first
-        // - put future meals before past meals
-        // TODO is this ok? Maybe also sort on availability ("available capacity")
-        var mealsCache = cache.get("meals");
-        mealsCache.push(meal);
-        console.log(mealsCache);
-        cache.set("meals", mealsCache.sort(function (a, b) {
-          var now = new Date().getTime();
-          var aTime = a[4];
-          var bTime = b[4];
-          if (aTime > now && bTime > now) { // both in the future
-            return aTime - bTime; // soon-est first (smallest timestamp)
-          } else { // either both in the past, or one in future and other in past
-            return bTime - aTime; // latest first and future over past (largest timestamp)
-          }
-        }));
-      });
+      var mealResult = await promisify(contractInstance.getMeal, i);
+      var meal = {};
+      meal.id = i;
+      meal.cook = mealResult[0];
+      meal.title = mealResult[1];
+      meal.description = mealResult[2];
+      meal.place = mealResult[3];
+      meal.time = mealResult[4].toNumber();
+      meal.price = mealResult[5].toNumber();
+      meal.capacity = mealResult[6].toNumber();
+      meal.reservations = mealResult[7];
+      // add the new meal to the meals cache, sorting the list by date 
+      // - for future meals put soon-est first
+      // - for past meals, put latest first
+      // - put future meals before past meals
+      // TODO is this ok? Maybe also sort on availability ("available capacity")
+      var mealsCache = cache.get("meals");
+      mealsCache.push(meal);
+      console.log(mealsCache);
+      cache.set("meals", mealsCache.sort(function (a, b) {
+        var now = new Date().getTime();
+        var aTime = a[4];
+        var bTime = b[4];
+        if (aTime > now && bTime > now) { // both in the future
+          return aTime - bTime; // soon-est first (smallest timestamp)
+        } else { // either both in the past, or one in future and other in past
+          return bTime - aTime; // latest first and future over past (largest timestamp)
+        }
+      }));
     }
   });
   //console.log(contractInstance.availableMeals.getData([0,1]));
